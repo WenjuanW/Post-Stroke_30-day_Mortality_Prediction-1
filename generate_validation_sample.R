52d03dfb7ab4bf6d2756839b286592c725ad3545
Wang <k1805276@kcl.ac.uk>
2021-03-03 10:57:59 +0000

    Updated the function for generating validation sample

--- generate_validation_sample.R
+++ generate_validation_sample.R
@@ -7,7 +7,7 @@
 n = 2000
 
 Age_Groups_by5 <- array(sample(21, n, replace = TRUE)) -1
-Male <- array(sample(c(0,1), n, replace = TRUE)) 
+Sex <- array(sample(c(0,1), n, replace = TRUE)) 
 
 #Generate a one hot encoding matrix of 6 category for ethnicity
 M_ethnicity <- matrix(0,n,6)
@@ -90,14 +90,15 @@ M_typestroke <- matrix(0,n,3)
 M_typestroke[cbind(1:n, sample(1:3, 3, rep=TRUE))] <- 1
 
 type_of_stroke.Primary.Intracerebral.Haemorrhage <- M_typestroke[,1] 
-type_of_stroke.Uknown  <- M_typestroke[,2] 
+type_of_stroke.Unknown  <- M_typestroke[,2] 
 
 
 mortality_30_day <- array(sample(c(0,1), n, replace = TRUE)) 
 
 
-validation_sample <- cbind(Age_Groups_by5, Male,
+validation_sample <- cbind(Age_Groups_by5, Sex,
                            Ethnicity.Black,Ethnicity.Mixed,Ethnicity.Other,Ethnicity.Unknown,Ethnicity.White,
+                           Inpatient_at_time_of_stroke,
                            hour_of_admission_4h_band.04.00.00.to.07.59.59,hour_of_admission_4h_band.08.00.00.to.11.59.59,hour_of_admission_4h_band.12.00.00.to.15.59.59,
                            hour_of_admission_4h_band.16.00.00.to.19.59.59,hour_of_admission_4h_band.20.00.00.to.23.59.59,
                            day_of_week_of_admission.Monday,day_of_week_of_admission.Saturday,day_of_week_of_admission.Sunday,
@@ -105,11 +106,12 @@ validation_sample <- cbind(Age_Groups_by5, Male,
                            congestive_heart_failure,hypertension,atrial_fibrillation,diabetes,previous_stroke_tia,
                            prior_anticoagulation_if_Afib.No.but,prior_anticoagulation_if_Afib.Unknown,prior_anticoagulation_if_Afib.Yes,
                            rankin_scale_prestroke,
-                           nihss_arrival,nihss_loss_of_consciousness,
+                           nihss_loss_of_consciousness,
                            nihss_answers_questions,nihss_obeys_commands,nihss_best_gaze,nihss_visual_deficits,nihss_facial_weakness,
                            nihss_left_arm_weakness,nihss_right_arm_weakness,nihss_left_leg_weakness,nihss_right_leg_weakness,
                            nihss_ataxia,nihss_sensory_loss,nihss_best_language,nihss_dysarthria,nihss_extinction,
-                           type_of_stroke.Primary.Intracerebral.Haemorrhage,type_of_stroke.Uknown,mortality_30_day)
+                           nihss_arrival,
+                           type_of_stroke.Primary.Intracerebral.Haemorrhage,type_of_stroke.Unknown,mortality_30_day)
 
 
-write.csv(validation_sample,"validation_sample")
+write.csv(validation_sample,"validation_sample.csv")
